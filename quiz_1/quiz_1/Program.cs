225a83321edac0741392926f87c765122868726f
GeraldFlores12 <60366178+GeraldFlores12@users.noreply.github.com>
2020-02-04 20:11:10 -0600

    prueba 2

---
+++ quiz_1/quiz_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace quiz_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = 0;
+            String entrada = "";
+
+            Console.Write("Ingrese el numero que desea: ");
+            entrada = Console.ReadLine();
+            num = Convert.ToDouble(entrada);
+            if (num >= 0)
+            {
+                Console.Write("EL NUMERO: {0}, ES POSITIVO",num);
+            }
+            else
+            {
+                Console.Write("EL NUMERO: {0}, ES NEGATIVO",num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
